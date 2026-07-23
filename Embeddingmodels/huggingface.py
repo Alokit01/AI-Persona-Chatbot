9aec6eefd46051bcfa3de21c6969a33ab908def0
Alokit  Raj Sharma <144568264+Alokit01@users.noreply.github.com>
2026-07-24 02:13:14 +0530

    Delete Embeddingmodels directory

--- Embeddingmodels/huggingface.py
+++
@@ -1,9 +0,0 @@
-from langchain_huggingface import HuggingFaceEmbeddings
-embedding=HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
-texts=["my name is alokit",
-       "I am fine "]
-vector = embedding.embed_documents(texts)
-print(len(vector))
-print(len(vector[1]))
